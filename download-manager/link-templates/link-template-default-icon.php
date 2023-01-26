2b56d39a452f15320c3a151810e56e679e224187
Mark <mark@mjbritton.co.uk>
2023-01-26 15:49:52 +0000

    download manager test

--- download-manager/link-templates/link-template-default-icon.php
+++ download-manager/link-templates/link-template-default-icon.php
@@ -3,7 +3,7 @@
   <a class="pull-left" href="[page_url]">
     [icon]
   </a>
-  <div class="media-body">
+  <div class="media-body">MJB
     <h3 class="media-heading m-0 p-0">[page_link]</h3>
     <div class="text-muted text-small"><i class="fas fa-copy"></i> [file_count] [txt=file(s)] <i
         class="fas fa-hdd ml-3"></i> [file_size]</div>
