72a3120cd847f3104a6174f8ea280172fbc1b23e
Mark <mark@mjbritton.co.uk>
2023-01-26 16:03:11 +0000

    Download manager update

--- download-manager/link-templates/link-template-default-icon.php
+++ download-manager/link-templates/link-template-default-icon.php
@@ -1,9 +1,9 @@
-<!-- WPDM Link Template: Default Template w/Icon - custom -->
-<div class="media">
+<!-- WPDM Link Template: Default Template w/Icon -->
+<div class="media mb-5">
   <a class="pull-left" href="[page_url]">
     [icon]
-  </a>
-  <div class="media-body">MJB
+     </a>
+   <div class="media-body ml-3">
     <h3 class="media-heading m-0 p-0">[page_link]</h3>
     <div class="text-muted text-small"><i class="fas fa-copy"></i> [file_count] [txt=file(s)] <i
         class="fas fa-hdd ml-3"></i> [file_size]</div>
